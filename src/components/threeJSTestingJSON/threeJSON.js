1fd6d817f94389f7f01791d0e6f3aa5b8b5517dd
Harkerfield <hartsfieldjwh@gmail.com>
2022-06-14 20:05:41 -0800

    1 file modified

--- src/components/threeJSTestingJSON/threeJSON.js
+++ src/components/threeJSTestingJSON/threeJSON.js
@@ -1,16 +1,25 @@
 import * as THREE from 'three';
 import React, { useState, useEffect, useReducer } from 'react';
+import Box from '@mui/material/Box';
 import { Canvas, useFrame } from '@react-three/fiber';
 
 function Test() {
   const [o1] = useState(
-    () => new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 'hotpink' })),
-  )
+    () =>
+      new THREE.Mesh(
+        new THREE.BoxGeometry(),
+        new THREE.MeshBasicMaterial({ color: 'hotpink' })
+      )
+  );
 
   const [o2] = useState(
-    () => new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 'aquamarine' })),
-  )
-  
+    () =>
+      new THREE.Mesh(
+        new THREE.BoxGeometry(),
+        new THREE.MeshBasicMaterial({ color: 'aquamarine' })
+      )
+  );
+
   // const [which, toggle] = useReducer((state) => !state, true)
   // useEffect(() => {
   //   const interval = setInterval(toggle, 1000)
@@ -22,13 +31,15 @@ function Test() {
   // })
 
   // return <primitive object={which ? o1 : o2} />
-  return <primitive object={o2} />
+  return <primitive object={o2} />;
 }
 
 export default function ThreeJSON() {
   return (
-    <Canvas>
-      <Test />
-    </Canvas>
-  )
+    <Box>
+      <Canvas>
+        <Test />
+      </Canvas>
+    </Box>
+  );
 }
