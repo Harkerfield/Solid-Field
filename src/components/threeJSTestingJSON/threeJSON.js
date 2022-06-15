3fa98bed6a15a8a2ae2174582c0bb1bd2732dc8f
Harkerfield <hartsfieldjwh@gmail.com>
2022-06-14 17:19:02 -0800

    1 file modified

--- src/components/threeJSTestingJSON/threeJSON.js
+++ src/components/threeJSTestingJSON/threeJSON.js
@@ -1,92 +1,73 @@
-import React from 'react';
-import * as THREE from 'three';
-import { Canvas, useFrame } from 'react-three-fiber';
+import * as THREE from 'three'
+import React, { useRef, useLayoutEffect, useState } from 'react'
+import { Canvas, useFrame, useThree, createPortal } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
+function Viewcube() {
+  const { gl, scene: defaultScene, camera: defaultCamera, size, events } = useThree()
+  const [scene] = useState(() => new THREE.Scene())
+  const [camera] = useState(() => new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000))
 
+  useLayoutEffect(() => {
+    camera.left = -size.width / 2
+    camera.right = size.width / 2
+    camera.top = size.height / 2
+    camera.bottom = -size.height / 2
+    camera.position.set(0, 0, 100)
+    camera.updateProjectionMatrix()
+  }, [size])
 
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hover, set] = useState<number | null>(null)
+  const matrix = new THREE.Matrix4()
 
+  useFrame(() => {
+    matrix.copy(defaultCamera.matrix).invert()
+    ref.current.quaternion.setFromRotationMatrix(matrix)
+    gl.autoClear = true
+    gl.render(defaultScene, defaultCamera)
+    gl.autoClear = false
+    gl.clearDepth()
+    gl.render(scene, camera)
+  }, 1)
 
-//https://github.com/pmndrs/react-three-fiber/blob/master/example/src/demos/Test.tsx
+  return (
+    <>
+      {createPortal(
+        <group>
+          <mesh
+            ref={ref}
+            position={[size.width / 2 - 120, size.height / 2 - 120, 0]}
+            onPointerOut={(e) => set(null)}
+            onPointerMove={(e) => set(Math.floor((e.faceIndex || 0) / 2))}>
+            {[...Array(6)].map((_, index) => (
+              <meshLambertMaterial
+                attach={`material-${index}`}
+                key={index}
+                color={hover === index ? 'hotpink' : 'white'}
+              />
+            ))}
+            <boxGeometry args={[80, 80, 80]} />
+          </mesh>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} intensity={0.5} />
+        </group>,
+        scene,
+        { camera, events: { priority: events.priority + 1 } },
+      )}
+    </>
+  )
+}
 
-
-
-
-
-
-import models from './cube.json';
-//On Windows by default both Chrome and Firefox use ANGLE based rendering backend.//
-
-export default function ThreeJSON(props) {
-  let camera, scene, renderer;
-  // let plane;
-  // let pointer, raycaster, isShiftDown = false;
-
-  // let rollOverMesh, rollOverMaterial;
-  // let cubeGeo, cubeMaterial;
-
-  const objects = [];
-
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  // const cube = new THREE.Mesh( geometry, material );
-  // scene.add( cube );
-
-  const loader = new THREE.ObjectLoader();
-  const cube = loader.parse(models);
-  scene.add(cube);
-  console.log('cube', cube);
-
-  camera.position.z = 5;
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-  }
-
-  animate();
-
-  // loader.load(
-  //   // resource URL
-  //   models,
-  //   // onLoad callback
-  //   // Here the loaded data is assumed to be an object
-  //   function (object) {
-  //     // Add the loaded object to the scene
-  //     scene.add(object);
-  //   },
-  //   // onProgress callback
-  //   function (xhr) {
-  //     console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-  //   },
-  //   // onError callback
-  //   function (err) {
-  //     console.error('An error happened');
-  //   }
-  // );
-  // Alternatively, to parse a previously loaded JSON structure
-
-  //document.body.appendChild(renderer.domElement);
-  const Rendertest = renderer.domElement;
-  // console.log("test", Rendertest);
-  document.body.appendChild(Rendertest);
-
-  // document.getElementById("renderingtest").appendChild(Rendertest);
-  // console.log('dom element', renderer.domElement);
-
-  return <div style={{ height: 500, width: '100%' }}></div>;
+export default function App() {
+  return (
+    <Canvas>
+      <mesh>
+        <torusGeometry args={[1, 0.5, 32, 100]} />
+        <meshNormalMaterial />
+      </mesh>
+      <Viewcube />
+      <OrbitControls />
+    </Canvas>
+  )
 }
