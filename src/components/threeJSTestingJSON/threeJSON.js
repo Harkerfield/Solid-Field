51024740c53c06fbb6e8556c0d3fdf0db585a125
Harkerfield <hartsfieldjwh@gmail.com>
2022-06-14 17:37:04 -0800

    1 file modified

--- src/components/threeJSTestingJSON/threeJSON.js
+++ src/components/threeJSTestingJSON/threeJSON.js
@@ -1,75 +1,166 @@
-//https://github.com/pmndrs/react-three-fiber/blob/master/example/src/demos/Viewcube.tsx
-
 import * as THREE from 'three'
-import React, { useRef, useLayoutEffect, useState } from 'react'
+import * as React from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Canvas, useFrame, useThree, createPortal } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import {
+  useGLTF,
+  PerspectiveCamera,
+  OrthographicCamera,
+  Environment,
+  OrbitControls,
+  ArcballControls,
+  TransformControls,
+  CameraShake,
+  Bounds,
+} from '@react-three/drei'
 
-function Viewcube() {
-  const { gl, scene: defaultScene, camera: defaultCamera, size, events } = useThree()
-  const [scene] = useState(() => new THREE.Scene())
-  const [camera] = useState(() => new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000))
-
-  useLayoutEffect(() => {
-    camera.left = -size.width / 2
-    camera.right = size.width / 2
-    camera.top = size.height / 2
-    camera.bottom = -size.height / 2
-    camera.position.set(0, 0, 100)
-    camera.updateProjectionMatrix()
-  }, [size])
-
-  const ref = useRef<THREE.Mesh>(!null)
-  const [hover, set] = useState<number | null>(null)
-  const matrix = new THREE.Matrix4()
-
-  useFrame(() => {
-    matrix.copy(defaultCamera.matrix).invert()
-    ref.current.quaternion.setFromRotationMatrix(matrix)
-    gl.autoClear = true
-    gl.render(defaultScene, defaultCamera)
-    gl.autoClear = false
-    gl.clearDepth()
-    gl.render(scene, camera)
-  }, 1)
-
+export function Soda(props: any) {
+  const [hovered, spread] = useHover()
+  const { nodes, materials } = useGLTF(
+    'https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/models/soda-bottle/model.gltf',
+  ) as any
   return (
-    <div>
-      {createPortal(
-        <group>
-          <mesh
-            ref={ref}
-            position={[size.width / 2 - 120, size.height / 2 - 120, 0]}
-            onPointerOut={(e) => set(null)}
-            onPointerMove={(e) => set(Math.floor((e.faceIndex || 0) / 2))}>
-            {[...Array(6)].map((_, index) => (
-              <meshLambertMaterial
-                attach={`material-${index}`}
-                key={index}
-                color={hover === index ? 'hotpink' : 'white'}
-              />
-            ))}
-            <boxGeometry args={[80, 80, 80]} />
-          </mesh>
-          <ambientLight intensity={0.5} />
-          <pointLight position={[10, 10, 10]} intensity={0.5} />
-        </group>,
-        scene,
-        { camera, events: { priority: events.priority + 1 } },
-      )}
-    </div>
-  )
-}
-
-export default function App() {
-  return (
-    <Canvas>
-      <mesh>
-        <torusGeometry args={[1, 0.5, 32, 100]} />
-        <meshNormalMaterial />
+    <group {...props} {...spread} dispose={null}>
+      <mesh geometry={nodes.Mesh_sodaBottle.geometry}>
+        <meshStandardMaterial color={hovered ? 'red' : 'green'} roughness={0} metalness={0.8} envMapIntensity={2} />
       </mesh>
-      <Viewcube />
-      <OrbitControls />
-    </Canvas>
+      <mesh geometry={nodes.Mesh_sodaBottle_1.geometry} material={materials.red} material-envMapIntensity={0} />
+    </group>
   )
 }
+
+function useHover() {
+  const [hovered, hover] = useState(false)
+  return [hovered, { onPointerOver: (e: any) => (e.stopPropagation(), hover(true)), onPointerOut: () => hover(false) }]
+}
+
+function View({ index = 1, children, clearColor, placement }: any) {
+  const { events, size } = useThree()
+  const [scene] = useState(() => new THREE.Scene())
+  const [position] = useState(() => new THREE.Vector2())
+  const [el] = useState(() => {
+    const div = document.createElement('div')
+    div.style.zIndex = index
+    div.style.position = 'absolute'
+    div.style.width = div.style.height = '50%'
+    return div
+  })
+
+  useEffect(() => {
+    switch (placement) {
+      case 'topright':
+        position.set(1, 1)
+        el.style.top = el.style.right = '0px'
+        break
+      case 'topleft':
+        position.set(0, 1)
+        el.style.top = el.style.left = '0px'
+        break
+      case 'bottomright':
+        position.set(1, 0)
+        el.style.bottom = el.style.right = '0px'
+        break
+      case 'bottomleft':
+      default:
+        position.set(0, 0)
+        el.style.bottom = el.style.left = '0px'
+        break
+    }
+  }, [placement])
+
+  useEffect(() => {
+    if (events.connected) {
+      const target = events.connected
+      target.appendChild(el)
+      return () => void target.removeChild(el)
+    }
+  }, [events, el])
+
+  const compute = useCallback((event, state) => {
+    if (event.target === el) {
+      const width = state.size.width
+      const height = state.size.height
+      state.pointer.set((event.offsetX / width) * 2 - 1, -(event.offsetY / height) * 2 + 1)
+      state.raycaster.setFromCamera(state.pointer, state.camera)
+    }
+  }, [])
+
+  return (
+    <>
+      {createPortal(
+        <Container index={index} clearColor={clearColor} position={position}>
+          {children}
+        </Container>,
+        scene,
+        {
+          events: { compute, priority: events.priority + index, connected: el },
+          size: { width: size.width / 2, height: size.height / 2 },
+        },
+      )}
+    </>
+  )
+}
+
+function Container({ children, index, clearColor, position }: any) {
+  const { size, camera, scene } = useThree()
+
+  useFrame((state) => {
+    const left = Math.floor(size.width * position.x)
+    const bottom = Math.floor(size.height * position.y)
+    const width = Math.floor(size.width)
+    const height = Math.floor(size.height)
+    state.gl.setViewport(left, bottom, width, height)
+    state.gl.setScissor(left, bottom, width, height)
+    state.gl.setScissorTest(true)
+    if (clearColor) state.gl.setClearColor(clearColor)
+    state.gl.render(scene, camera)
+  }, index)
+
+  return <>{children}</>
+}
+
+const App = () => (
+  <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 5] }}>
+    <View index={1} placement="bottomleft" clearColor={new THREE.Color('orange').convertLinearToSRGB()}>
+      <OrthographicCamera makeDefault zoom={100} />
+      <Scene preset="lobby" />
+      <OrbitControls makeDefault />
+    </View>
+    <View index={2} placement="bottomright" clearColor={new THREE.Color('hotpink').convertLinearToSRGB()}>
+      <PerspectiveCamera makeDefault />
+      <Scene preset="city" />
+      <ArcballControls />
+    </View>
+    <View index={3} placement="topleft" clearColor={new THREE.Color('aquamarine').convertLinearToSRGB()}>
+      <PerspectiveCamera makeDefault />
+      <Scene preset="dawn" />
+      <OrbitControls makeDefault />
+      <CameraShake intensity={2} />
+    </View>
+    <View index={4} placement="topright" clearColor={new THREE.Color('lightblue').convertLinearToSRGB()}>
+      <PerspectiveCamera makeDefault />
+      <Scene preset="warehouse" />
+      <OrbitControls makeDefault />
+      <TransformControls scale={3} position={[0, -0.75, 2]}>
+        <Soda />
+      </TransformControls>
+    </View>
+  </Canvas>
+)
+
+function Scene({ children, controls = true, preset }: any) {
+  return (
+    <Bounds fit clip observe>
+      <ambientLight intensity={1} />
+      <pointLight position={[20, 30, 10]} />
+      <pointLight position={[-10, -10, -10]} color="blue" />
+      <Soda scale={3} position={[-1, -0.75, 1]} />
+      <Soda scale={3} position={[1, -0.75, 1]} />
+      <Soda scale={3} position={[0, -0.75, 0]} />
+      <Environment preset={preset} />
+      {children}
+    </Bounds>
+  )
+}
+
+export default App
