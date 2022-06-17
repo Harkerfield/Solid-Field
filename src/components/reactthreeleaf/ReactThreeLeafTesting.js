7a7b413a9a32879ecf36ca9824717f71fd47987e
Harkerfield <hartsfieldjwh@gmail.com>
2022-06-16 22:26:17 -0800

    2 files modified

--- src/components/reactthreeleaf/ReactThreeLeafTesting.js
+++ src/components/reactthreeleaf/ReactThreeLeafTesting.js
@@ -1,12 +1,11 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Stats, OrbitControls, CameraShake } from "@react-three/drei";
-
+import { Stats, OrbitControls, CameraShake } from '@react-three/drei';
 
 function Box(props) {
-  const mesh = useRef(null)
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
+  const mesh = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
   // useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
   return (
     <mesh
@@ -15,32 +14,23 @@ function Box(props) {
       scale={active ? 1.5 : 1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
+      onPointerOut={(event) => setHover(false)}
+    >
       <boxGeometry args={[1.5, 1.5, 1.5]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
-  )
+  );
 }
 
-
-
-
-
-
 export default function App() {
-
-  
-
   return (
     <div>
-    <Canvas>
-
-      <ambientLight />
-
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
-  )
+  );
 }
