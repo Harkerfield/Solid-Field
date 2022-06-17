28e7b9fb48e4b9b1739a4857541a199b2dd76f6f
Harkerfield <hartsfieldjwh@gmail.com>
2022-06-16 23:32:01 -0800

    1 file modified

--- src/components/reactthreeleaf/ReactThreeLeafTesting.js
+++ src/components/reactthreeleaf/ReactThreeLeafTesting.js
@@ -16,7 +16,7 @@ function Box(props) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
-      <boxGeometry args={[50, 50, 50]} />
+      <boxGeometry args={[50,50,50]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   );
@@ -26,19 +26,19 @@ export default function App() {
   return (
     <div>
       {/* <Canvas onCreated={(state) => state.gl.setClearColor('blue')}> */}
-      <Canvas camera={{ fov: 75, position: [-10, 45, 20] }}>
+      <Canvas camera={{position: [100, 300, 500] }}>
         <camera rotation={[500, 800, 1300]} />
         <raycaster />
         <vector2 />
         <gridHelper args={[1000, 20]} />
-        <color attach="background" args={['red']} />
+        <color attach="background" args={['0xf0f0f0 ']} />
         <ambientLight />
         {/* 
         <directionalLight position={[10, 10, 10]} /> */}
         {/* <planeGeometry rotate="{ - Math.PI / 2}" /> */}
         <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <Box position={[0, 0, 0]} />
+        <Box position={[50, 0, 0]} />
       </Canvas>
     </div>
   );
