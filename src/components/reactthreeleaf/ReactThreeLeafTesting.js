d8847905686487a121bba630ffd1677b2d8921db
Harkerfield <hartsfieldjwh@gmail.com>
2022-06-16 20:17:41 -0800

    1 file modified

--- src/components/reactthreeleaf/ReactThreeLeafTesting.js
+++ src/components/reactthreeleaf/ReactThreeLeafTesting.js
@@ -1,11 +1,35 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Stats, OrbitControls, CameraShake } from "@react-three/drei";
+
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+
+
+
+function WobbleCamera() {
+  const shakeRef = useRef();
+  const orbitRef = useRef();
+  useEffect(() => {
+    orbitRef.current.addEventListener("change", () => {
+      const shake = shakeRef.current.getIntensity();
+      shakeRef.current.setIntensity(shake + 0.015);
+    });
+  }, [orbitRef]);
+
+  return (
+    <>
+      <OrbitControls ref={orbitRef} />
+      <CameraShake ref={shakeRef} additive decay />
+    </>
+  );
+}
+
 
 function Box(props) {
   const mesh = useRef(null)
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
   return (
     <mesh
       {...props}
@@ -14,17 +38,38 @@ function Box(props) {
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
+      <boxGeometry args={[1.5, 1.5, 1.5]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
 
+
+const renderer = await ReactThreeTestRenderer.create(
+  <mesh>
+    <boxBufferGeometry args={[2, 2]} />
+    <meshStandardMaterial
+      args={[
+        {
+          color: 0x0000ff,
+        },
+      ]}
+    />
+  </mesh>,
+)
+
+
+
 export default function App() {
+
+  
+
   return (
     <div>
     <Canvas>
+
       <ambientLight />
+
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
